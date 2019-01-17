4349bd087590ce3a1bae6d353c1d3cd4e335b7fa
xiaobaoxixi <liyan.xiaobaoxixi@gmail.com>
2019-01-17 06:57:03 +0100

    Added: nav and hero section on frontpage

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const navAnchorS = document.querySelectorAll("nav a:not(.lang)");
+navAnchorS.forEach(a => {
+  a.addEventListener("click", e => {
+    e.preventDefault();
+    hightlightAnchor(e.target);
+    showContent(e.target.dataset.section);
+  });
+});
+
+function hightlightAnchor(elem) {
+  if (elem.classList.contains("chosen")) {
+    elem.classList.remove("chosen");
+  } else {
+    if (document.querySelector(".chosen")) {
+      document.querySelector(".chosen").classList.remove("chosen");
+    }
+    elem.classList.add("chosen");
+  }
+}
+
+function showContent(section) {
+  if (section) {
+    console.log(section);
+  }
+}
