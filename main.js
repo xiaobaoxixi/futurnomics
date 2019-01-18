0401dcc716b3da8cce619046d3744636d8b2b3ee
xiaobaoxixi <liyan.xiaobaoxixi@gmail.com>
2019-01-18 12:31:36 +0100

    added: pause and resume switching circles

--- main.js
+++ main.js
@@ -22,6 +22,47 @@ function init() {
     }
   }
 
+  const descCircleS = document.querySelectorAll(".desc-circle");
+  const partnerCircleS = document.querySelectorAll(".partner-circle");
+  const redDot = document.querySelector(".red-dot-wrapper");
+  descCircleS.forEach(c => {
+    c.addEventListener("mouseenter", stopAnimation);
+  });
+  partnerCircleS.forEach(c => {
+    c.addEventListener("mouseenter", stopAnimation);
+  });
+  descCircleS.forEach(c => {
+    c.addEventListener("mouseleave", resumeAnimation);
+  });
+  partnerCircleS.forEach(c => {
+    c.addEventListener("mouseleave", resumeAnimation);
+  });
+
+  function stopAnimation() {
+    descCircleS.forEach(c => {
+      c.style.WebkitAnimationPlayState = "paused";
+      c.style.animationPlayState = "paused";
+    });
+    partnerCircleS.forEach(c => {
+      c.style.WebkitAnimationPlayState = "paused";
+      c.style.animationPlayState = "paused";
+    });
+    redDot.style.WebkitAnimationPlayState = "paused";
+    redDot.style.animationPlayState = "paused";
+  }
+  function resumeAnimation() {
+    descCircleS.forEach(c => {
+      c.style.WebkitAnimationPlayState = "running";
+      c.style.animationPlayState = "";
+    });
+    partnerCircleS.forEach(c => {
+      c.style.WebkitAnimationPlayState = "running";
+      c.style.animationPlayState = "";
+    });
+    redDot.style.WebkitAnimationPlayState = "running";
+    redDot.style.animationPlayState = "";
+  }
+
   function showContent(section) {
     if (section) {
       console.log(section);
