9d243e0ae4c45a0a7088e25fdf51d9e108837b8f
xiaobaoxixi <liyan.xiaobaoxixi@gmail.com>
2019-01-18 11:45:37 +0100

    added: switch between circles with CSS

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 "use strict";
 
-const navAnchorS = document.querySelectorAll("nav a:not(.lang)");
-navAnchorS.forEach(a => {
-  a.addEventListener("click", e => {
-    e.preventDefault();
-    hightlightAnchor(e.target);
-    showContent(e.target.dataset.section);
+window.addEventListener("DOMContentLoaded", init);
+function init() {
+  const navAnchorS = document.querySelectorAll("nav a:not(.lang)");
+  navAnchorS.forEach(a => {
+    a.addEventListener("click", e => {
+      e.preventDefault();
+      hightlightAnchor(e.target);
+      showContent(e.target.dataset.section);
+    });
   });
-});
 
-function hightlightAnchor(elem) {
-  if (elem.classList.contains("chosen")) {
-    elem.classList.remove("chosen");
-  } else {
-    if (document.querySelector(".chosen")) {
-      document.querySelector(".chosen").classList.remove("chosen");
+  function hightlightAnchor(elem) {
+    if (elem.classList.contains("chosen")) {
+      elem.classList.remove("chosen");
+    } else {
+      if (document.querySelector(".chosen")) {
+        document.querySelector(".chosen").classList.remove("chosen");
+      }
+      elem.classList.add("chosen");
     }
-    elem.classList.add("chosen");
   }
-}
 
-function showContent(section) {
-  if (section) {
-    console.log(section);
+  function showContent(section) {
+    if (section) {
+      console.log(section);
+    }
   }
 }
