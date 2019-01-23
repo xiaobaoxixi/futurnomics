2b6019e2cd6fc41af82193322a87a10c6c60693c
xiaobaoxixi <liyan.xiaobaoxixi@gmail.com>
2019-01-23 17:48:29 +0100

    added: steps switch

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 window.addEventListener("DOMContentLoaded", init);
 function init() {
+  // content selection when page reloads
   let section = window.localStorage.getItem("section");
   if (section) {
     showContent(section);
@@ -9,7 +10,7 @@ function init() {
       .querySelector(`[data-section="${section}"]`)
       .classList.add("chosen");
   }
-
+  // navigation and content selection when nav is clicked
   const navAnchorS = document.querySelectorAll("nav a:not(.lang)");
   navAnchorS.forEach(a => {
     a.addEventListener("click", e => {
@@ -17,7 +18,6 @@ function init() {
       showContent(e.target.dataset.section);
     });
   });
-
   function hightlightAnchor(elem) {
     if (elem.classList.contains("chosen")) {
       elem.classList.remove("chosen");
@@ -28,7 +28,40 @@ function init() {
       elem.classList.add("chosen");
     }
   }
+  function showContent(classKeyword) {
+    if (
+      classKeyword === "entre" ||
+      classKeyword === "intra" ||
+      classKeyword === "founder"
+    ) {
+      window.localStorage.setItem("section", classKeyword);
+    } else if (classKeyword === "home") {
+      window.localStorage.removeItem("section");
+    } else {
+      console.log(classKeyword);
+    }
 
+    if (classKeyword) {
+      document.querySelector(".hero").classList.remove("close");
+      document.querySelector(".hero").classList.remove("half");
+      document
+        .querySelectorAll(`.content:not(.${classKeyword}-content)`)
+        .forEach(c => {
+          c.classList.add("hide");
+        });
+      document.querySelectorAll(`.${classKeyword}-content`).forEach(s => {
+        s.classList.remove("hide");
+      });
+    }
+    if (classKeyword === "founder") {
+      document.querySelector(".hero").classList.add("close");
+    }
+    if (classKeyword === "entre" || classKeyword === "intra") {
+      document.querySelector(".hero").classList.add("half");
+    }
+  }
+
+  // "work with" animation
   const descCircleS = document.querySelectorAll(".desc-circle");
   const partnerCircleS = document.querySelectorAll(".partner-circle");
   const redDot = document.querySelector(".red-dot-wrapper");
@@ -69,37 +102,31 @@ function init() {
     redDot.style.WebkitAnimationPlayState = "running";
     redDot.style.animationPlayState = "";
   }
-
-  function showContent(classKeyword) {
-    if (
-      classKeyword === "entre" ||
-      classKeyword === "intra" ||
-      classKeyword === "founder"
-    ) {
-      window.localStorage.setItem("section", classKeyword);
-    } else if (classKeyword === "home") {
-      window.localStorage.removeItem("section");
-    } else {
-      console.log(classKeyword);
-    }
-
-    if (classKeyword) {
-      document.querySelector(".hero").classList.remove("close");
-      document.querySelector(".hero").classList.remove("half");
-      document
-        .querySelectorAll(`.content:not(.${classKeyword}-content)`)
-        .forEach(c => {
-          c.classList.add("hide");
+  // animation steps
+  const allStepS = document.querySelectorAll(".step");
+  const allStepDescS = document.querySelectorAll(".step-desc");
+  allStepS.forEach(s => {
+    const stepNr = s.dataset.step;
+    s.addEventListener("mouseenter", e => {
+      allStepS.forEach(as => {
+        as.style.zIndex = "initial";
+      });
+      allStepDescS.forEach(asd => {
+        asd.classList.add("hide");
+      });
+      document.querySelector(`.step-desc-${stepNr}`).classList.remove("hide");
+      e.target.style.zIndex = "5";
+      e.target.setAttribute("src", `img/${stepNr}-text.png`);
+    });
+    if (stepNr !== "one") {
+      s.addEventListener("mouseleave", e => {
+        e.target.style.zIndex = "initial";
+        e.target.setAttribute("src", `img/${stepNr}-without-text.png`);
+        allStepDescS.forEach(asd => {
+          asd.classList.add("hide");
         });
-      document.querySelectorAll(`.${classKeyword}-content`).forEach(s => {
-        s.classList.remove("hide");
+        document.querySelector(`.step-desc-one`).classList.remove("hide");
       });
     }
-    if (classKeyword === "founder") {
-      document.querySelector(".hero").classList.add("close");
-    }
-    if (classKeyword === "entre" || classKeyword === "intra") {
-      document.querySelector(".hero").classList.add("half");
-    }
-  }
+  });
 }
